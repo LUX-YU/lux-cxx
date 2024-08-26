163aebecac4e499320905be6484d0f63056bbe28
chenhui.lux.yu <chenhui.lux.yu@outlook.com>
2024-08-26 17:05:23 +0100

    Updated.

--- static_reflection/include/lux/cxx/static_reflection/static_type_info.hpp
+++ static_reflection/include/lux/cxx/static_reflection/static_type_info.hpp
@@ -9,20 +9,55 @@ namespace lux::cxx
 {
     template<typename T, typename... Args> struct static_type_info;
 
-    template<typename TypeInfo, typename Str>
+    template<auto Field, typename TypeInfo, typename Str>
     requires is_ct_string_v<Str>
     struct field_info
     {
-        static_assert(is_type_template_of_v<static_type_info, TypeInfo>, "T must be static type info type");
+        template<typename U, typename T>
+        static constexpr auto extract_owner(T U::*member) noexcept -> U;
+
+        using owner      = decltype(extract_owner(Field));
         using type_info  = TypeInfo;
         using name       = Str;
+
+        static inline constexpr type_info::type& get(owner& _owner) noexcept
+        requires std::is_member_object_pointer_v<decltype(Field)>
+        {
+            return _owner.*Field;
+        }
+
+        static inline constexpr const type_info::type& get(const owner& _owner) noexcept
+        requires std::is_member_object_pointer_v<decltype(Field)>
+        {
+            return _owner.*Field;
+        }
+
+        template<typename... Args>
+        static inline constexpr auto invoke(owner& _owner, Args&&... args)
+        requires std::is_member_function_pointer_v<decltype(Field)>
+        {
+            return (_owner.*Field)(std::forward<Args>(args)...);
+        }
     };
 
-    template<typename T> concept field_info_type = is_type_template_of_v<field_info, T>;
+    template<template<auto,typename,typename> class T, class U>
+    class is_field_info_of
+    {
+    private:
+        template<auto Ptr, typename... _TPack>
+        static constexpr auto _d(T<Ptr, _TPack...>*) -> std::true_type;
+        static constexpr auto _d(...) -> std::false_type;
+    public:
+        static constexpr bool value = decltype(_d((U*)nullptr))::value;
+    };
+
+    template<typename T> static inline constexpr bool is_field_info_of_v = is_field_info_of<field_info, T>::value;
+    template<typename T> concept field_info_type = is_field_info_of_v<T>;
 
     template<typename T, typename... Args>
     struct static_type_info
     {
+        using type   = T;
         using fields = std::tuple<Args...>;
         static_assert(
             ((field_info_type<Args> || is_type_template_of_v<static_type_info, Args>) && ...), 
@@ -60,13 +95,45 @@ namespace lux::cxx
         {
             tuple_traits::for_each_type<fields>(std::forward<FUNC>(func));
         }
+
+        /* FUNC: only accept template lambda and template function now
+		 * example:
+		 * static_type_info::for_each_field<test_tuple>(
+		 *     []<field_info_type T, size_t I>(T&& obj)
+		 *     {
+		 *			// ...
+		 *     }
+		 * );
+		*/
+        template<typename FUNC, typename Obj>
+        static constexpr void for_each_field(FUNC&& _func, Obj&& _obj) noexcept
+        {
+            tuple_traits::for_each_type<fields>(
+                [func = std::forward<FUNC>(_func), obj = std::forward<Obj>(_obj)]<typename U, size_t I>() mutable
+                {
+                    func.template operator()<U, I>(std::forward<Obj>(obj));
+                }
+            );
+        }
     };
 
     template<typename T> concept static_type_info_type = is_type_template_of_v<static_type_info, T>;
 }
 
-#define MAKE_FIELD_TYPE(type, field_name)\
-    ::lux::cxx::field_info<::lux::cxx::static_type_info<type>, MAKE_CT_STRING_TYPE(#field_name)>
+#define MAKE_FIELD_TYPE(owner, type, field_name)\
+    ::lux::cxx::field_info<&owner::field_name, ::lux::cxx::static_type_info<type>, MAKE_CT_STRING_TYPE(#field_name)>
 
-#define MAKE_FIELD_TYPE_EX(info, field_name)\
-    ::lux::cxx::field_info<info, MAKE_CT_STRING_TYPE(#field_name)>
+#define MAKE_FIELD_TYPE_EX(owner, info, field_name)\
+    ::lux::cxx::field_info<&owner::field_name, info, MAKE_CT_STRING_TYPE(#field_name)>
+
+#define FIELD_TYPE(name)\
+    MAKE_FIELD_TYPE(_type, ::lux::cxx::static_type_info<decltype(_type::name)>, name)
+
+#define FIELD_TYPE_EX(info, name)\
+    MAKE_FIELD_TYPE_EX(_type, info, name)
+
+#define START_STATIC_TYPE_INFO_DECLARATION(type)\
+    decltype(\
+        ([](){using _type = type; return ::lux::cxx::static_type_info<type, \
+
+#define END_STATIC_TYPE_INFO_DECLARATION() >{};}()))
--- static_reflection/test/static_reflection_test.cpp
+++ static_reflection/test/static_reflection_test.cpp
@@ -1,12 +1,24 @@
 #include "lux/cxx/static_reflection/static_type_info.hpp"
 #include <iostream>
 #include <string>
+#include <functional>
 
 struct test_type
 {
     int     a;
     double  b;
+
+    double func(double a, std::string b)
+    {
+        return a + b.size();
+    }
 };
+
+std::ostream& operator<<(std::ostream& os, const test_type& td)
+{
+    os << td.a << ' ' << td.b;
+    return os;
+}
 
 struct test_type2
 {
@@ -19,18 +31,26 @@ struct test_type2
 using test_type_info = 
 lux::cxx::static_type_info<
     test_type,
-    MAKE_FIELD_TYPE(int, a),
-    MAKE_FIELD_TYPE(double, b)
+    MAKE_FIELD_TYPE(test_type, int, a),
+    MAKE_FIELD_TYPE(test_type, double, b)
 >;
 
 using test_type_info2 = 
 lux::cxx::static_type_info<
     test_type2,
-    MAKE_FIELD_TYPE(int, a),
-    MAKE_FIELD_TYPE(double, b),
-    MAKE_FIELD_TYPE(std::string, str),
-    MAKE_FIELD_TYPE_EX(test_type_info, test)
+    MAKE_FIELD_TYPE(test_type2, int, a),
+    MAKE_FIELD_TYPE(test_type2, double, b),
+    MAKE_FIELD_TYPE(test_type2, std::string, str),
+    MAKE_FIELD_TYPE_EX(test_type2, test_type_info, test)
 >;
+
+using test_type_info3 = 
+START_STATIC_TYPE_INFO_DECLARATION(test_type2)
+    FIELD_TYPE(a),
+    FIELD_TYPE(b),
+    FIELD_TYPE(str),
+    FIELD_TYPE_EX(test_type_info, test)
+END_STATIC_TYPE_INFO_DECLARATION();
 
 template<typename T, size_t I, size_t Depth = 0>
 void print_field() requires lux::cxx::field_info_type<T>
@@ -57,6 +77,21 @@ int main(int argc, char* argv[])
         {
             print_field<T, I>();
         }
+    );
+
+    test_type2 obj;
+    obj.a       = 10;
+    obj.b       = 20.0;
+    obj.str     = "Hello, World!";
+    obj.test.a  = 30;
+    obj.test.b  = 40.0;
+
+    test_type_info2::for_each_field(
+        []<typename T, size_t I>(test_type2& info)
+        {
+            std::cout << T::get(info) << std::endl;      
+        },
+        obj
     );
 
     return 0;
